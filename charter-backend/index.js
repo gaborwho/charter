0eb61a6924491eb31f66073bc34ed28aca5e7ce7
Jerovetz <jerovetz@comlions.net>
2020-02-13 19:10:38 +0100

    add socket access and http

--- charter-backend/index.js
+++ charter-backend/index.js
@@ -1,7 +1,27 @@
 const Koa = require('koa');
 const app = new Koa(); 
+const socket = (require('koa-websocket'))(app);
+const router = require('koa-router');
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-}); 
-app.listen(3000);
+const http = router();
+const ws = router();
+
+http.get('/', ctx => {
+    ctx.status = 200;
+    ctx.body = 'Hello!';
+});
+
+ws.get('/socket', ctx => {
+	console.log('nem routolja');
+    ctx.websocket.send('Hey!');
+    ctx.websocket.on('message', (message) => {
+        console.log(message);
+    });	
+});
+
+app.use(http.routes()).use(http.allowedMethods());
+socket.ws.use(ws.routes()).use(ws.allowedMethods());
+
+socket.listen(3000);
+
+
