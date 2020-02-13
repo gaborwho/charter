e26ed3a79e32f41ba2e0bbf7d4d911246700a150
Gabor Balla <cavalierrant@gmail.com>
2020-02-13 19:50:25 +0100

    add input endpoint

--- charter-backend/index.js
+++ charter-backend/index.js
@@ -2,26 +2,43 @@ const Koa = require('koa');
 const app = new Koa(); 
 const socket = (require('koa-websocket'))(app);
 const router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const EventEmitter = require('events');
 
 const http = router();
 const ws = router();
 
+const myEmitter = new EventEmitter();
+
 http.get('/', ctx => {
-    ctx.status = 200;
-    ctx.body = 'Hello!';
-    
+  ctx.status = 200;
+  ctx.body = 'Hello!';
+});
+
+http.post('/input', ctx => {
+  console.log(ctx.request.body);
+  myEmitter.emit('event', ctx.request.body);
+  ctx.status = 200;
 });
 
 ws.get('/socket', ctx => {
-	console.log('nem routolja');
-    ctx.websocket.send('Hey!');
-    ctx.websocket.on('message', (message) => {
-        console.log(message);
-    });	
+  myEmitter.on('event', string => {
+    console.log('event received');
+    console.log(string);
+    ctx.websocket.send(JSON.stringify(string));
+ });
+
+  ctx.websocket.send('Hey!');
+  ctx.websocket.on('message', (message) => {
+      console.log(message);
+  });
+
 });
 
-app.use(http.routes()).use(http.allowedMethods());
+app.use(bodyParser());
+app.use(http.routes());//.use(http.allowedMethods());
 socket.ws.use(ws.routes()).use(ws.allowedMethods());
+
 
 socket.listen(3000);
 
