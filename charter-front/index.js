7c842b855dc287c6a38efbbd9656cb64381c5efe
Gabor Balla <cavalierrant@gmail.com>
2020-03-05 18:10:31 +0100

    chart data and dinamically load datasets

--- charter-front/index.js
+++ charter-front/index.js
@@ -5,30 +5,41 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgba(255, 99, 132, 0.4)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        },
-        {        
-            label: 'My Second dataset',
-            backgroundColor: 'rgba(155, 99, 132, 0.9)',
-            borderColor: 'rgb(155, 99, 132)',
-            data: [10, 110, 25, 32, 420, 230, 445]
-        }
-        ]
+        labels: [],
+        datasets: []
     },
     options: {}
 });
 var i = 0;
 
-setInterval(() => {
-	console.log(chart.data.datasets[0].data);
-	chart.data.labels.push("dataset " + i);
-	i++;
-	chart.data.datasets[0].data.push(Math.round(Math.random() * 100));
-	chart.data.datasets[1].data.push(Math.round(Math.random() * 200));
-	chart.update();
-}, 1000);
+var backendSocket = new WebSocket("ws://localhost:3000/socket");
+
+const updateDatasets = data => {
+  Object.keys(data).forEach(datasetName => {
+    if (chart.data.datasets.every(chartDataset => {
+      return chartDataset.label !== datasetName;
+      })) {
+      chart.data.datasets.push({
+        label: datasetName,
+        backgroundColor: 'rgba(155, 99, 132, 0.9)',
+        borderColor: 'rgb(155, 99, 132)',
+        data: []
+      });
+    }
+  });
+};
+
+backendSocket.onmessage = function (event) {
+  const data = JSON.parse(event.data);
+  updateDatasets(data);
+
+  chart.data.labels.push("dataset " + i);
+  i++;
+
+  Object.keys(data).forEach(datasetName => {
+    console.log(data);
+    chart.data.datasets.find(dataset => dataset.label === datasetName).data.push(data[datasetName]);
+  });
+
+  chart.update();
+};
